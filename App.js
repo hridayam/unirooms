dc130ffa4e37b66f18d23540538de5e1e566664b
hridayam <hridayambakshi2011@gmail.com>
2018-09-14 13:37:19 -0700

    minor bug fix

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import firebase from 'firebase';
 
 import { 
     AuthScreen, 
