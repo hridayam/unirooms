1eafcd2a038e4b341309ccdff1e7641def5e30c6
hridayam <hridayambakshi2011@gmail.com>
2018-10-13 04:31:59 -0700

    connect to firebase and login implementation started

--- uniroom-client/src/actions/auth.js
+++ uniroom-client/src/actions/auth.js
@@ -1,3 +1,11 @@
-export const login = () => {
+import firebase from 'firebase';
 
+export const loginUser = (credentials) => async dispatch => {
+    const { email, password } = credentials;
+    try {
+        const user = await firebase.auth().signInWithEmailAndPassword(email, password);
+        console.log(user);
+    } catch (err) {
+        console.log(err);
+    }
 };
